34834ee240c5d373280a73746a46bc2947a970ba
SwaggyDonut <zhangduona1995@gmail.com>
2024-09-15 00:02:36 -0400

    update

--- models/data_mart/dim_inventory.sql
+++ models/data_mart/dim_inventory.sql
@@ -1,30 +1,32 @@
 {{ config(
-    materialized='incremental',
-    unique_key='inventory_id'
+   materialized='incremental',
+   unique_key='inventory_id'
 ) }}
 
+
 WITH inventory_data AS (
-    SELECT
-        i.inventory_id,
-        i.reorder_level,
-        i.product_id,
-        i.quantity_in_stock,
-        i.warehouse_id,
-        i.rating,
-        i.stock_level,
-        i.sales_volume,
-        i.restock_date,
-        i.weight,
-        i.supplier_id,
-        i.discounts,
-        i.storage_condition,
-        i.safety_stock,
-        i.inventory_status,
-        i.average_monthly_demand,
-        i.last_audit_date,
-        i.last_restock_date,
-        i.next_restock_date
-    FROM {{ source('de_project', 'inventory_data') }} i
+   SELECT
+       i.inventory_id,
+       i.reorder_level,
+       i.product_id,
+       i.quantity_in_stock,
+       i.warehouse_id,
+       i.rating,
+       i.stock_level,
+       i.sales_volume,
+       i.restock_date,
+       i.weight,
+       i.supplier_id,
+       i.discounts,
+       i.storage_condition,
+       i.safety_stock,
+       i.inventory_status,
+       i.average_monthly_demand,
+       i.last_audit_date,
+       i.last_restock_date,
+       i.next_restock_date
+   FROM {{ source('de_project', 'inventory_data') }} i
 )
+
 
 SELECT * FROM inventory_data
