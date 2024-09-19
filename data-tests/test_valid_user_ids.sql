3231efaa3a58c87fa6eaaaa3d7e6eca06a19d039
SwaggyDonut <zhangduona1995@gmail.com>
2024-09-18 22:00:49 -0400

    add tests

---
+++ data-tests/test_valid_user_ids.sql
@@ -0,0 +1,7 @@
+-- Ensure all user_ids in the model exist in the valid_users source
+SELECT
+  u.user_id
+FROM {{ ref('fact_user_engagement') }}
+LEFT JOIN {{ source('de_project', 'user_data') }} u
+ON {{ ref('fact_user_engagement') }}.user_id = u.user_id
+WHERE u.user_id IS NULL
