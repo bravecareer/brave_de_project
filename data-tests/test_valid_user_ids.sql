2101dbc320a76e584a1acdfc3677d04f1290ad0c
aorosoeon <arsen.chuz@gmail.com>
2024-10-12 14:47:33 -0400

    completed more or less everything before step 5 apache airflow, dbt created tables and pushed them to snowflake

--- data-tests/test_valid_user_ids.sql
+++ data-tests/test_valid_user_ids.sql
@@ -1,7 +1,7 @@
 -- Ensure all user_ids in the model exist in the valid_users source
 SELECT
   u.user_id
-FROM {{ ref('fact_user_engagement') }}
+FROM {{ ref('fact_user_engagement_arsen_c') }}
 LEFT JOIN {{ source('de_project', 'user_data') }} u
-ON {{ ref('fact_user_engagement') }}.user_id = u.user_id
+ON {{ ref('fact_user_engagement_arsen_c') }}.user_id = u.user_id
 WHERE u.user_id IS NULL
