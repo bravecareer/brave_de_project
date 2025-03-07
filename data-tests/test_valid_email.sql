33d95fcf6f8445a4ee1bf59a1eaba10317da61e0
bibhumlai <bibhu@MSI.>
2025-03-07 16:08:01 -0500

    dev_start

--- data-tests/test_valid_email.sql
+++ data-tests/test_valid_email.sql
@@ -2,7 +2,7 @@ with invalid_emails as (
   select
     user_id,
     email
-  from {{ ref('dim_user') }}
+  from {{ ref('dim_user_BS') }}
   where email not like '%_@__%.__%'
 )
 select * from invalid_emails
