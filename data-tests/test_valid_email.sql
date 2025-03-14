dfdab01a7615e94a7197aac002c801fbb4b08cd7
Joby Chacko <jobychacko2001@gmail.com>
2025-03-14 01:10:06 -0400

    Initial commit of brave_de_project-Joby

--- data-tests/test_valid_email.sql
+++
@@ -1,8 +0,0 @@
-with invalid_emails as (
-  select
-    user_id,
-    email
-  from {{ ref('dim_user') }}
-  where email not like '%_@__%.__%'
-)
-select * from invalid_emails
--- data-tests/test_valid_product_ids.sql
+++
@@ -1,7 +0,0 @@
--- Ensure all product_ids in the model exist in the valid_products source
-SELECT
-  p.product_id
-FROM {{ ref('fact_user_transaction') }}
-LEFT JOIN {{ source('de_project', 'product_data') }} p
-ON {{ ref('fact_user_transaction') }}.product_id = p.product_id
-WHERE p.product_id IS NULL
