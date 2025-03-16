9fb86592e3a4321cf285dd34163b468d3a7a96b5
Brian Li <brianzcli@gmail.com>
2025-03-16 17:17:59 -0400

    Initial commit

---
+++ models/data_mart/dim_product_data_bl.sql
@@ -0,0 +1,24 @@
+{{ config(
+   materialized='incremental',
+   unique_key='product_id'
+) }}
+
+
+WITH product_data AS (
+   SELECT
+       p.product_id,
+       p.product_name,
+       p.product_category,
+       p.price,
+       p.product_color,
+       p.manufacturing_date,
+       p.expiration_date,
+       p.warranty_period,
+       p.rating,
+       p.weight_grams,
+       p.discount_percentage
+   FROM {{ source('de_project', 'product_data') }} p
+)
+
+
+SELECT * FROM product_data
---
+++ models/data_mart/fact_product_performance_bl.sql
@@ -0,0 +1,12 @@
+WITH (
+
+),
+
+SELECT 
+       product_id,
+       product_name
+       price,
+       views,
+       atc_events,
+       purchases
+  FROM 
