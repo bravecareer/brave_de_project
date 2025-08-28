fd76576bc3531695c1599607344800afae06c626
JocelynCui <40900447+cuij2@users.noreply.github.com>
2025-08-28 10:24:38 -0400

    Revert "pulling from the main branch"

---
+++ models/data_mart/dim_inventory_jc.sql
@@ -0,0 +1,33 @@
+{{ config(
+   materialized='view',
+   unique_key='inventory_id'
+) }}
+
+
+WITH inventory_data AS (
+   SELECT
+       i.inventory_id,
+       i.reorder_level,
+       i.product_id,
+       i.quantity_in_stock,
+       i.warehouse_id,
+       i.rating,
+       i.stock_level,
+       i.sales_volume,
+       i.restock_date,
+       i.weight,
+       i.supplier_id,
+       i.discounts,
+       i.storage_condition,
+       i.safety_stock,
+       i.inventory_status,
+       i.average_monthly_demand,
+       i.last_audit_date,
+       i.last_restock_date,
+       i.next_restock_date
+   FROM {{ source('de_project', 'inventory_data') }} i
+   WHERE last_audit_date >= CURRENT_DATE() - 5 
+)
+
+
+SELECT * FROM inventory_data
